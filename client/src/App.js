973d2a38ed7df4c7154c9d9824720fb12a1c3e56
JPPUERTAORREGO <160149085+JPPUERTAORREGO@users.noreply.github.com>
2024-02-22 11:26:59 -0500

    creacion de divs para trabajo colaborativo

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
-
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { HeaderNav } from './components/HeaderNav';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hola Juli, siempre firmes!!</h1>
+      <div className='HeaderNav'>{/* div para trabajo de juan pablo*/}
+        <HeaderNav/>
+
+
+        
+      </div>
+      <div>{/* div para trabajo de Juliana */}
+        
+        
+        
+        </div>
+      <div>{/* div para trabajo de Kevin */}
+        
+        
+        </div>
     </div>
   );
 }
