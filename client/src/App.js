9ed00f4f28581c9a8d545e81d7ca1d193922c4e3
KEV-8A <133246538+KEV-8A@users.noreply.github.com>
2024-02-22 16:48:45 -0500

    se agrega paginacion al footer
    directamente traido de bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { HeaderNav } from './components/HeaderNav';
+import FooterPages from './components/FooterPages'
 
 function App() {
   return (
     <div className="App">
       <div className='HeaderNav'>{/* div para trabajo de juan pablo*/}
-        <HeaderNav/>
+        <HeaderNav />
 
 
-        
+
       </div>
       <div>{/* div para trabajo de Juliana */}
-        
-        
-        
-        </div>
-      <div>{/* div para trabajo de Kevin */}
-        
-        
-        </div>
+
+
+
+      </div>
+      <div className='FooterPages'>{/* div para trabajo de Kevin */}
+        <FooterPages/>
+
+      </div>
     </div>
   );
 }
